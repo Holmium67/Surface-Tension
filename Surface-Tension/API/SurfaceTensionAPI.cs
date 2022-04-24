d44c6f59a44b8c330525af703d202a2f3bd4d1ed
Dylan Morris <dylanmorris330@gmail.com>
2022-04-24 12:30:38 +0100

    Updated plugin to Exiled v5.1.3

---
+++ Surface-Tension/API/SurfaceTensionAPI.cs
@@ -0,0 +1,35 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System.Collections.Generic;
+
+namespace Surface_Tension.API
+{
+    public static class SurfaceTensionAPI
+    {
+        public static float DamageCalculation(Player player)
+        {
+            if (Plugin.Instance.Config.IsDamagePercent)
+                return (player.MaxHealth / 100) * Plugin.Instance.Config.Damage;
+            return Plugin.Instance.Config.Damage;
+        }
+
+        public static IEnumerator<float> DealDamage()
+        {
+            while (true)
+            {
+                foreach (Player player in Player.List)
+                {
+                    if (!player.IsAlive)
+                        continue;
+
+                    if (player.Health > DamageCalculation(player))
+                        player.Health -= DamageCalculation(player);
+                    else
+                        player.Kill("Died to radiation.");
+                }
+
+                yield return Timing.WaitForSeconds(Plugin.Instance.Config.TimeBetweenDamage);
+            }
+        }
+    }
+}
---
+++ Surface-Tension/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using Exiled.API.Features;
+using Server = Exiled.Events.Handlers.Server;
+using Warhead = Exiled.Events.Handlers.Warhead;
+using System;
+
+namespace Surface_Tension
+{
+    public class Plugin : Plugin<Config>
+    {
+        public static Plugin Instance;
+        private EventHandler events;
+
+        public override string Name => "SurfaceTenshion";
+        public override string Author => "Holmium67, updated by Heisenberg3666";
+        public override Version Version => new Version(2, 0, 0, 0);
+        public override Version RequiredExiledVersion => new Version(5, 1, 3);
+
+        public override void OnEnabled()
+        {
+            base.OnEnabled();
+            Instance = this;
+            events = new EventHandler();
+            RegisterEvents();
+        }
+
+        public override void OnDisabled()
+        {
+            UnregisterEvents();
+            events = null;
+            Instance = null;
+            base.OnDisabled();
+        }
+
+        public void RegisterEvents()
+        {
+            Server.EndingRound += events.OnRoundEnd;
+            Warhead.Detonated += events.OnWarheadDetonation;
+        }
+
+        public void UnregisterEvents()
+        {
+            Server.EndingRound -= events.OnRoundEnd;
+            Warhead.Detonated -= events.OnWarheadDetonation;
+        }
+    }
+}
